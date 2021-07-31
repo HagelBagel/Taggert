2d94df4b8376860e6dd309ac658c89d41e9eeb16
Christian <chfrom@chfrom.dk>
2021-07-31 15:27:27 +0200

    Can now add and remove a single tag item from tag-list

--- app/assets/scripts/modules/TagAdder.js
+++ app/assets/scripts/modules/TagAdder.js
@@ -10,16 +10,31 @@ class TagAdder {
             this.addTheTag();
             e.preventDefault();
         });
+
+        document.querySelector(".tag-list").addEventListener("click", e => {
+            if (e.target.tagName.toLowerCase() === 'span') {
+                console.log("Removing the tag");                
+                this.removeTheTag();
+                e.preventDefault();
+            }            
+        });
     }
 
+    
     addTheTag() {               
         console.log(this.tagInput);  
         document.getElementById("tag-list").innerHTML = `
-            <span class="tag-list__item"> 
+            <span class="tag-list__item" id="tag-item"> 
                 <span class="tag-list__item__text">${this.tagInput}</span>
-                <span class="tag-list__item__close">x</span>
+                <span class="tag-list__item__close id="remove-tag">x</span>
             </span>`;  
-        document.getElementById("submit-box").reset();     
+        document.getElementById("submit-box").reset();
+        
+    }
+
+    removeTheTag() {
+        this.tagItem = document.getElementById("tag-item");        
+        this.tagItem.remove();
     }
 }
 
