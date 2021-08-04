cf0e7da76f9988da304355392c0d16ddd90e17b7
Christian <chfrom@chfrom.dk>
2021-08-04 11:53:07 +0200

    Can now add multiple tags using comma-separation

--- app/assets/scripts/modules/TagAdder.js
+++ app/assets/scripts/modules/TagAdder.js
@@ -27,12 +27,16 @@ class TagAdder {
         let i;      
         for (i = 0; i < tagArray.length; i++) {
             let tag = tagArray[i];
-            console.log("Added " + tag);  
-            document.getElementById("tag-list").innerHTML = `
-            <span class="tag-list__item" id="${tag}"> 
+            console.log("Added " + tag);
+            let newTag = document.createElement('span');
+            newTag.classList.add('tag-list__item');
+            newTag.id = tag;
+            newTag.innerHTML = `            
                 <span class="tag-list__item__text">${tag}</span>
                 <span class="tag-list__item__close id="remove-tag">x</span>
-            </span>`;  
+            `;   
+            console.log(newTag); 
+            document.getElementById("tag-list").prepend(newTag);
         };
         
         document.getElementById("submit-box").reset();        
